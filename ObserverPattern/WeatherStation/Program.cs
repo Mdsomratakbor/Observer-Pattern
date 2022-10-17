98dd588c4d317002148f3d673a0ff28209dcdca4
Md Somrat Akbor <somrat876@gmail.com>
2022-10-18 00:43:30 +0600

    currentcondition display implement

--- ObserverPattern/WeatherStation/Program.cs
+++ ObserverPattern/WeatherStation/Program.cs
@@ -1,4 +1,5 @@
-﻿using WeatherStation.subjects;
+﻿using WeatherStation.observers;
+using WeatherStation.subjects;
 
 Console.WriteLine("Welcome to the Weather O Rama Station");
 Console.WriteLine("Enter {temparature} {humidity} {pressure}");
@@ -6,5 +7,6 @@ var data =  Console.ReadLine();
 
 
 WeatherData weather = new WeatherData();
-
+CurrentConditionsDisplay currentConditions = new CurrentConditionsDisplay(weather);
 weather.SetMeasurements((float)data[0], (float)(data[1]), (float)(data[2]));
+
